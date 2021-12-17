a3b8d1a11ce78a6e4692bf8e6b6a2eb89dc13cbc
Nakaishi Ryosuke <acna2676@gmail.com>
2021-12-17 10:34:49 +0900

    99. rendering optimization

--- frontend/mobile/MemoApp/src/components/MemoList.jsx
+++ frontend/mobile/MemoApp/src/components/MemoList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  View, Text, StyleSheet, TouchableOpacity, Alert
+  View, Text, StyleSheet, TouchableOpacity, Alert, FlstList
 } from 'react-native';
 // import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
@@ -13,18 +13,16 @@ import MemoEditScreen from "../screens/MemoEditScreen";
 export default function MemoList(){
   // const {memos} = props;
   const navigation = useNavigation();
+
+  function renderItem({item}){
     return (
-      <View>
-        {/* {MemoEditScreen.map((memo)=>{
-          return (
-            <TouchableOpacity 
-            key={memo.id}
+      <TouchableOpacity 
             style={styles.memoListItem}
             onPress = {()=> {navigation.navigate('MemoDetail');}}
             >
             <View>
-              <Text style={styles.memoListItemTitle}>{memo.bodyText}</Text>
-              <Text style={styles.memoListItemDate}>string({memo.updatedAt})</Text>
+              <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
+              <Text style={styles.memoListItemDate}>string({item.updatedAt})</Text>
             </View>
             <TouchableOpacity 
             style={styles.memoDelete}
@@ -32,37 +30,32 @@ export default function MemoList(){
               <Icon name="delete" size={24} color='#B0B0B0' />
             </TouchableOpacity>
           </TouchableOpacity>
-          )
-        })} */}
-        <TouchableOpacity 
-          style={styles.memoListItem}
-          onPress = {()=> {navigation.navigate('MemoDetail');}}
-          >
-          <View>
-            <Text style={styles.memoListItemTitle}>Shopping List</Text>
-            <Text style={styles.memoListItemDate}>2021/12/13 12:00:00</Text>
-          </View>
-          <TouchableOpacity 
-          style={styles.memoDelete}
-          onPress={()=>{Alert.alert('Are you sure?')}}>
-            <Icon name="delete" size={24} color='#B0B0B0' />
-            {/* <Feather name="x" size={16} color='#B0B0B0' /> */}
-            {/* <Text>X</Text> */}
-          </TouchableOpacity>
-        </TouchableOpacity>
+    )
+  }
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={memos}
+          renderItem={renderItem}
+          keyExtractor={(item)=>item.id}
+        />
       </View>
 );
 }
 
-// MemoList.prototype={
-//   memos: arrayOf(shape({
-//     id: string,
-//     bodyText: string,
-//     updatedAt: instanceOf(Date),
-//   })).isRequired,
-// }
+MemoList.prototype={
+  memos: arrayOf(shape({
+    id: string,
+    bodyText: string,
+    updatedAt: instanceOf(Date),
+  })).isRequired,
+}
 
 const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
     memoListItem: {
         backgroundColor:'#ffffff',
         flexDirection:'row',
